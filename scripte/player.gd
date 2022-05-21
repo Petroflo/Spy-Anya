4c55c0697c9800bffa16e4f9db74a846938b20d5
Petroflo <petro.floresku@epitech.eu>
2022-05-21 21:53:04 +0200

    [ADD] scene scriptes for new foncdtionnalities

--- scripte/player.gd
+++ scripte/player.gd
@@ -3,18 +3,37 @@ extends KinematicBody2D
 var velocity : Vector2 = Vector2()
 var dirrection: Vector2 = Vector2()
 
+
+var active = false
+
+func _ready():
+	connect("body_entered", self, '_on_spy_body_entered')
+	connect("body_exited", self, '_on_spy_body_exited')
+
+func _process(delta):
+
+	$sprite_question_mark.visible = active
+
+func _on_spy_body_entered(body):
+	if body.name == 'spy':
+		active = true
+
+func _on_spy_body_exited(body):
+	if body.name == 'spy':
+		active = false
+
 func read_input():
 	velocity = Vector2()
-	if Input.is_action_pressed("down"):
+	if Input.is_action_pressed("down") or Input.is_action_pressed("ui_down"):
 		velocity.y += 1
 		$AnimationPlayer.play("Walk_down")
-	elif Input.is_action_pressed("up"):
+	elif Input.is_action_pressed("up") or Input.is_action_pressed("ui_up"):
 		velocity.y -= 1
 		$AnimationPlayer.play("Walk_back")
-	elif Input.is_action_pressed("left"):
+	elif Input.is_action_pressed("left") or Input.is_action_pressed("ui_left"):
 		velocity.x -= 1
 		$AnimationPlayer.play("Walk_left")
-	elif Input.is_action_pressed("right"):
+	elif Input.is_action_pressed("right") or Input.is_action_pressed("ui_right"):
 		velocity.x += 1
 		$AnimationPlayer.play("Walk_right")
 	else:
