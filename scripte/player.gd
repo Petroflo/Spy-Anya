9cb0b6acfb63739eacf4a154e3df83fb114f6d22
Petroflo <petro.floresku@epitech.eu>
2022-05-21 15:46:39 +0200

    [ADD] animation of Anya

--- scripte/player.gd
+++ scripte/player.gd
@@ -5,14 +5,20 @@ var dirrection: Vector2 = Vector2()
 
 func read_input():
 	velocity = Vector2()
-	if Input.is_action_pressed("up"):
-		velocity.y -= 1
 	if Input.is_action_pressed("down"):
 		velocity.y += 1
-	if Input.is_action_pressed("left"):
+		$AnimationPlayer.play("Walk_down")
+	elif Input.is_action_pressed("up"):
+		velocity.y -= 1
+		$AnimationPlayer.play("Walk_back")
+	elif Input.is_action_pressed("left"):
 		velocity.x -= 1
-	if Input.is_action_pressed("right"):
+		$AnimationPlayer.play("Walk_left")
+	elif Input.is_action_pressed("right"):
 		velocity.x += 1
+		$AnimationPlayer.play("Walk_right")
+	else:
+		$AnimationPlayer.stop() 
 		
 	velocity = velocity.normalized()
 	velocity = move_and_slide(velocity * 200)
