c48ba8421de31ae2171aebb37d234307247bfd10
Bentaleb Adam <adam.bentaleb@epitech.eu>
2022-05-21 14:58:42 +0200

    [ADD] movements

---
+++ scripte/player.gd
@@ -0,0 +1,20 @@
+extends KinematicBody2D
+
+var velocity : Vector2 = Vector2()
+var dirrection: Vector2 = Vector2()
+
+func read_input():
+	velocity = Vector2()
+	if Input.is_action_pressed("up"):
+		velocity.y -= 1
+	if Input.is_action_pressed("down"):
+		velocity.y += 1
+	if Input.is_action_pressed("left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("right"):
+		velocity.x += 1
+		
+	velocity = velocity.normalized()
+	velocity = move_and_slide(velocity * 200)
+func _physics_process(delta):
+	read_input()
