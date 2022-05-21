786b014ca539dbd6e86cab7d4856051b66b2a529
Bentaleb Adam <adam.bentaleb@epitech.eu>
2022-05-21 17:22:14 +0200

    [ADD] spy and question mark

---
+++ spy.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var active = false
+
+func _ready():
+	connect("body_entered", self, '_on_spy_body_entered')
+	connect("body_exited", self, '_on_spy_body_exited')
+
+func _process(delta):
+
+	$sprite_question_mark.visible = active
+
+func _on_spy_body_entered(body):
+	if body.name == 'player':
+		active = true
+
+func _on_spy_body_exited(body):
+	if body.name == 'player':
+		active = false
